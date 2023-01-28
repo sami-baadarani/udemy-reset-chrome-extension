27747753e9bc16f29ec0318eebfd996b9b9f90d9
Sami Baadarani <samibaadarani@gmail.com>
2023-01-28 21:37:40 +1100

    feat: 🚀 project init

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('reset').addEventListener('click', function () {
+    // document.getElementById('myForm').reset();
+    console.log('reset')
+    window.location.reload();
+});

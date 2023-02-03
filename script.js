2f01264848bf557d55ec15c86f1baa891ee00a3e
Sami Baadarani <samibaadarani@gmail.com>
2023-02-03 21:44:40 +1100

    chore: add example of working alert on button click

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const resetBtn = document.querySelector('#reset');
 
 resetBtn.addEventListener('click', function () {
-    console.log('reset')
+    console.log('reset')   
 });
+
+// resetBtn.person_name = "Bob";
+// const greeting = "hello, ";
+// resetBtn.addEventListener(
+//     "click", () => alert(greeting + resetBtn.person_name + "."), false);
